681b5a3534688ff4aab2f589a0d39617aac960e7
JohnSec974 <jh.sec974@gmail.com>
2020-04-04 22:00:16 +0400

    Create simple struct handler for config file

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,33 @@ fn main() {
     println!("Hello, world!");
 }
 
+
+struct Config {
+	config_name: String,
+	folder_path: String,
+	file_name: String,
+	last_line: u32
+}
+
+
+struct EmailMedia {
+	address: String,
+	password: String,
+	host: String,
+	port: u32,
+	use_tls: bool,
+	subject_prefix: String
+}
+
+impl Config {
+	fn new() {
+	
+	}
+}
+
+impl EmailMedia {
+	fn send() {
+	
+	}
+}
+
